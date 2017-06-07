a44d1aff34f8fa9912f2fdaa158b128c0f89ba97
jiexunsee <jxsee94@yahoo.com.sg>
2017-06-08 00:52:03 +0800

    update

--- adamoptimizer.py
+++ adamoptimizer.py
@@ -2,7 +2,6 @@
 """
 with reference to https://arxiv.org/pdf/1412.6980v8.pdf
 """
-import numpy as np
 
 class AdamOptimizer:
     def __init__(self, weights, alpha=0.001, beta1=0.9, beta2=0.999, epsilon=1e-8):
@@ -14,7 +13,7 @@ class AdamOptimizer:
         self.v = 0
         self.t = 0
         self.theta = weights
-        
+
     def backward_pass(self, gradient):
         self.t = self.t + 1
         self.m = self.beta1*self.m + (1 - self.beta1)*gradient
@@ -23,4 +22,3 @@ class AdamOptimizer:
         v_hat = self.v/(1 - self.beta2**self.t)
         self.theta = self.theta - self.alpha*(m_hat/(np.sqrt(v_hat) - self.epsilon))
         return self.theta
-    
